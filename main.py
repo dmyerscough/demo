32197027bf9dfc176f3621fc520fb0b6a7c8d933
Damian Myerscough <Damian.Myerscough@gmail.com>
2020-05-09 23:46:25 -0700

    Clean up code and introduce a bug without running unit tests

--- main.py
+++ main.py
@@ -3,18 +3,10 @@
 
 def fib():
     """
-
+    Fibonacci numbers
     """
     a, b = 0, 1
 
     while True:
-        yield a
         a, b = b, a + b
-
-
-if __name__ == '__main__':
-
-    s = fib()
-
-    for i in range(0, 11):
-        print(next(s))
+        yield a
